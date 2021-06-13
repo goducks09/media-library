c18f9b65c0f58733e35b8633332b927b594863d1
Chris <none>
2021-06-13 13:40:58 -0700

    Server deployed to Heroku

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/userRoutes";
 require('dotenv').config();
     
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //if there are cors errors, npm i cors
 //app.use(cors());
